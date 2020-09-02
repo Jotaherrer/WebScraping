e1ecb6ddcad6881457ced96e33abe85355403cda
JulianHerrera <herrera.ajulian@gmail.com>
2020-09-02 17:50:08 -0300

    update on w2v model

--- w2v.py
+++ w2v.py
@@ -1,10 +1,27 @@
-import logging, gensim
+import logging, gensim, os
 
-logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s",
-                    datefmt= '%H:%M:%S', level=logging.INFO)
+def get_raw_text(path, html=False):
+    """
+    Extracts text from the notes
+    """
+    if html:
+                
+    else:
+        raw_text = {}
+        articles = os.listdir(path)
+        for number, art in enumerate(articles):
+            with open(path+art) as arhivo:
+                raw_text[number] = archivo.read()
+        return raw_text
 
-                    
-TRAIN_PATH_LN = "./datos_clase_03/la_nacion_data/articles_data/"
 
 if __name__ =='__main__':
-    
+
+    logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s",
+                        datefmt= '%H:%M:%S', level=logging.INFO)
+                  
+
+
+    TRAIN_PATH_LN = "./datos_clase_03/la_nacion_data/articles_data/"
+
+    articles = os.listdir(TRAIN_PATH_LN)
