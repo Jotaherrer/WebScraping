6ad711708f2db51ebf66a4ffa970c878ce3ea604
JulianHerrera <herrera.ajulian@gmail.com>
2020-09-02 17:37:06 -0300

    word2vec model - v1

---
+++ w2v.py
@@ -0,0 +1,10 @@
+import logging, gensim
+
+logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s",
+                    datefmt= '%H:%M:%S', level=logging.INFO)
+
+                    
+TRAIN_PATH_LN = "./datos_clase_03/la_nacion_data/articles_data/"
+
+if __name__ =='__main__':
+    
