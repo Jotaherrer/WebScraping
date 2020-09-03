d8e8b4b6907aad24c187b59e9ad8556c13cc4c3d
JulianHerrera <herrera.ajulian@gmail.com>
2020-09-02 21:24:28 -0300

    update on work2vec model

--- w2v.py
+++ w2v.py
@@ -1,27 +1,131 @@
-import logging, gensim, os
+import logging, gensim, os, pickle
+from nltk.tokenize import sent_tokenize, word_tokenize
+
 
 def get_raw_text(path, html=False):
     """
     Extracts text from the notes
     """
     if html:
-                
+        with open('ln_tokens_tr', 'rb') as archivo:
+            raw_text = pickle.load(archivo)
     else:
         raw_text = {}
         articles = os.listdir(path)
         for number, art in enumerate(articles):
-            with open(path+art) as arhivo:
+            with open(path+art, encoding='utf-8') as archivo:
                 raw_text[number] = archivo.read()
+            if number % 1000 == 0:
+                print(f'Successfully printed note number {number}')
         return raw_text
 
 
-if __name__ =='__main__':
+def tokenize_newspaper_news(text):
+    """
+    Tokenization of words included in the news. Returns a list of lists,
+    with the words tokenized. The first lists includes every note in the 
+    newspaper
+    """
+    tokens_list = []
+    for n, value in text.items():
+        sentences = sent_tokenize(value)
+        sentences = [word_tokenize(e) for e in sentences if (e != ',') | (e != '.')]
+        tokens_list.append(sentences)
+        if n % 500 == 0:
+            print(f'Sucessfully stored note number {n}')
 
+    sentence_list = []
+    for note in tokens_list:
+        for sent in note:
+            sentence_list.append(sent)
+        
+    return sentence_list
+
+
+def word_2_vec_model(raw_sentences, win=5):
+    """
+    Performs the W2V model with the predeteremined parameters. I left the
+    window size of the context as the only parameter of the model to change.
+    """
+    ### Defining parameters
+    model = gensim.models.Word2Vec(size=50,
+                                   window=win,
+                                   min_count=5,
+                                   negative=5,
+                                   sample=0.01,
+                                   workers=4,
+                                   sg=1)
+    ### Creating vocabulary
+    model.build_vocab(raw_sentences, 
+                      progress_per=10000)
+    ### Training the model
+    model.train(raw_sentences, 
+                total_examples=model.corpus_count,
+                epochs=20,
+                report_delay=1)
+    ### Saving model
+    model.save('W2V_project.model')
+    return model
+
+
+if __name__ =='__main__':
     logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s",
                         datefmt= '%H:%M:%S', level=logging.INFO)
                   
+    train_path_ln = "./datos_clase_03/la_nacion_data/articles_data/"
 
+    ### Collecting data
+    data = get_raw_text(train_path_ln)
+    tokens = tokenize_newspaper_news(data)
+    
+    ### W2V model - 5 windows
+    w2v_model = word_2_vec_model(tokens)
+    # Vectores
+    w2v_model.__getitem__(['gato'])
+    w2v_model.__getitem__(['blanco'])
+    # Similarity between words
+    w2v_model.wv.similarity('blanco', 'negro')
+    w2v_model.wv.similarity('plata', 'negro')
+    w2v_model.wv.similarity('Macri', 'presidente')
+    w2v_model.wv.similarity('Cristina', 'presidenta')
+    # More similar words
+    w2v_model.wv.most_similar(positive=['Macri'], topn=10)
+    w2v_model.wv.most_similar(positive=['tecnología'], topn=10)
+    w2v_model.wv.most_similar(positive=['felicidad'], topn=10)
+    w2v_model.wv.most_similar(positive=['miedo'], topn=10)
+    w2v_model.wv.most_similar(positive=['trabajo'], topn=10)
+    w2v_model.wv.most_similar(positive=['Libre'], topn=10)
+    # Analogies
+    w2v_model.wv.most_similar(positive=['Queen'], negative=['música'], topn=5)
+    w2v_model.wv.most_similar(positive=['Queen', 'música'], topn=5)
+    w2v_model.wv.most_similar(positive=['Maradona'], negative=['fútbol'], topn=5)
+    # Out of context word
+    w2v_model.wv.doesnt_match(['Galicia', 'Frances', 'casa'])
+    w2v_model.wv.doesnt_match(['Bolsonaro', 'Trump', 'Biden'])
+    
 
-    TRAIN_PATH_LN = "./datos_clase_03/la_nacion_data/articles_data/"
-
-    articles = os.listdir(TRAIN_PATH_LN)
+    ### W2V model - 10 windows
+    w2v_model_large = word_2_vec_model(tokens, 10)
+    # Vectores
+    w2v_model_large.__getitem__(['gato'])
+    w2v_model_large.__getitem__(['blanco'])
+    # Similarity between words
+    w2v_model_large.wv.similarity('blanco', 'negro')
+    w2v_model_large.wv.similarity('plata', 'negro')
+    w2v_model_large.wv.similarity('Macri', 'presidente')
+    w2v_model_large.wv.similarity('Cristina', 'presidenta')
+    # More similar words
+    w2v_model_large.wv.most_similar(positive=['Macri'], topn=10)
+    w2v_model_large.wv.most_similar(positive=['tecnología'], topn=10)
+    w2v_model_large.wv.most_similar(positive=['felicidad'], topn=10)
+    w2v_model_large.wv.most_similar(positive=['miedo'], topn=10)
+    w2v_model_large.wv.most_similar(positive=['trabajo'], topn=10)
+    w2v_model_large.wv.most_similar(positive=['Libre'], topn=10)
+    # Analogies
+    w2v_model_large.wv.most_similar(positive=['Queen'], negative=['música'], topn=5)
+    w2v_model_large.wv.most_similar(positive=['Queen', 'música'], topn=5)
+    w2v_model_large.wv.most_similar(positive=['Maradona'], negative=['fútbol'], topn=5)
+    # Out of context word
+    w2v_model_large.wv.doesnt_match(['Galicia', 'Frances', 'casa'])
+    w2v_model_large.wv.doesnt_match(['Bolsonaro', 'Trump', 'Biden'])
+    
